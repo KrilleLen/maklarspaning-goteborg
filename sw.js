4658d9d5e8af7696b0d23774743bdd47a4fbd467
KrilleLen <christoffer.lennartsson@erikolsson.se>
2026-07-24 22:10:18 +0200

    Ersätt självraderande cache med version 7.3

--- sw.js
+++ sw.js
@@ -1,22 +1,37 @@
-const VERSION = 'maklarspaning-self-destruct-v7-2';
+const CACHE = 'maklarspaning-v7-3-20260724';
+const CORE = [
+  './',
+  './index.html',
+  './styles.css?v=7.3',
+  './app.js?v=7.3',
+  './manifest.webmanifest?v=7.3',
+  './favicon.svg',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './data/app-data.json?v=7.3'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(self.skipWaiting());
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.map(key => caches.delete(key)));
-    await self.clients.claim();
-    const clients = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
-    await Promise.all(clients.map(client => client.navigate('./latest.html?v=7.2')));
-    await self.registration.unregister();
-  })());
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method === 'GET') {
-    event.respondWith(fetch(event.request, { cache: 'no-store' }));
-  }
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))
+  );
 });
